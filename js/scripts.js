7c1b268f3c3aaf06dec667d4e5de07da2ad2aa43
maconrgarilao <mariaxricafort@gmail.com>
2022-08-01 10:59:38 -0400

    Correction on the format

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 let pokemonList = [
-  {name: 'Butterfree', height: 1.1, type: ['bug', 'flying']},
-  {name: 'Cyndaquil', height 0.5, type: 'fire'},
-  {name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal']},
-  [
+  { name: 'Butterfree', 
+    height: 1.1, 
+    type: ['bug', 'flying']},
+  
+  { name: 'Cyndaquil', 
+    height: 0.5, 
+    type: 'fire'},
 
+  { name: 'Jigglypuff', 
+    height: 0.5, 
+    type: ['fairy', 'normal']},
+]
