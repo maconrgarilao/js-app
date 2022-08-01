f1ede6fe8515089501b244dabc1be4517eef2d35
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2022-08-01 10:32:23 -0400

    Added pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+let pokemonList = [
+  {name: 'Butterfree', height: 1.1, type: 'bug', 'flying'},
+  {name: 'Cyndaquil', height 0.5, type: 'fire'},
+  {name: 'Jigglypuff', height: 0.5, type: 'fairy', 'normal'},
+  [
