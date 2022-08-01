cbd0971d8183e5f0e0352c3de754962dfa9678f0
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2022-08-01 10:37:33 -0400

    Added [ ] to the string of array in type

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-  {name: 'Butterfree', height: 1.1, type: 'bug', 'flying'},
+  {name: 'Butterfree', height: 1.1, type: ['bug', 'flying']},
   {name: 'Cyndaquil', height 0.5, type: 'fire'},
-  {name: 'Jigglypuff', height: 0.5, type: 'fairy', 'normal'},
+  {name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal']},
   [
+
