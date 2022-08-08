8c09f9a68c67eabfe3bbb99cde8741e0ca8081b3
maconrgarilao <mariaxricafort@gmail.com>
2022-08-08 12:38:49 -0400

    Created a loop and conditions

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,13 @@ let pokemonList = [
     type: ['fairy', 'normal']},
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
+for (let i = 0; i < pokemonList.height; i++) {
     if (pokemonList[i].height > 1) {
         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!')
-    }
+    } else if (pokemonList[i].height <= 1 && pokemonList[i].height > 0.5) {
+    } else if (pokemonList[i].height <= 1 && pokemonList[i].height > 0.5) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - That\'s a decent size.')
+    } else if (pokemonList[i].height < 0.5) {
+    } else if (pokemonList[i].height < 0.5) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].weight + ') - That\'s a cute size!')
+    }}
