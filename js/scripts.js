91672fe8348c860d4212ad91eaf1c668cc39638c
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2022-08-08 12:15:05 -0400

    Created a loop

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,8 @@ let pokemonList = [
     height: 0.5, 
     type: ['fairy', 'normal']},
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!')
+    {
