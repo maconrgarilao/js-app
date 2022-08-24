4df77951895536e4c10c43cd6cbc3198a8ff9a65
maconrgarilao <mariaxricafort@gmail.com>
2022-08-24 10:24:15 -0400

    Wrapped the pokemonList array to an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-let pokemonList = [
-  { name: 'Butterfree', 
+let pokemonRepository = (function () {
+  let   pokemonList = [
+  
+    { name: 'Butterfree', 
     height: 1.1, 
     type: ['bug', 'flying']},
   
@@ -10,9 +12,26 @@ let pokemonList = [
   { name: 'Jigglypuff', 
     height: 0.4, 
     type: ['fairy', 'normal']},
-]
+    
+  ];
+  
+  function getAll() {
+      return pokemonList;
+  }
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
 
-//for (let i = 0; i < pokemonList.length; i++) {
+  return {
+    getAll: getAll,
+    add: add,
+  }
+})();
+
+let pokemonList = pokemonRepository.getAll();
+
+//    FOR REFERENCE ONLY - 'for' loop 
+//    for (let i = 0; i < pokemonList.length; i++) {
 //    if (pokemonList[i].height > 1) {
 //        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '</p>')
 //    } else if (pokemonList[i].height <= 1 && pokemonList[i].height >= 0.5) {
@@ -21,6 +40,16 @@ let pokemonList = [
 //      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - That\'s a cute size!' + '</p>')
 //    }}
 
+//    FOR REFERENCE - 'forEach' loop
+
 pokemonList.forEach(function(pokemon) {
-  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + 'm high and is a ' + pokemon.type + ' pokemon!' + '</p>' )
-});
+  if (pokemon.height > 1) {
+  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + 'm high and is a ' + pokemon.type + ' pokemon!' + ' - Wow, that\'s big!' + '</p>')
+} else if (pokemon.height <= 1 && pokemon.height >= 0.5) {
+  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + 'm high and is a ' + pokemon.type + ' pokemon!' + ' - That\'s a decent size!' + '</p>')
+} else if (pokemon.height < 0.5) {
+  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + 'm high and is a ' + pokemon.type + ' pokemon!' + ' - That\'s a cute size!' + '</p>')
+}
+})
+
+console.log(pokemonRepository.getAll());
