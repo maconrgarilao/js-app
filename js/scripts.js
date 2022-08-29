1a10b40ce3ffa5d9356d53b9f1ca66f292821f0c
maconrgarilao <mariaxricafort@gmail.com>
2022-08-29 11:19:14 -0400

    Added a loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,22 @@ let pokemonRepository = (function () {
     console.log(pokemon);
   };
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error (e)
+    })
+  };
+
   return {
     add: add,
     getAll: getAll,
